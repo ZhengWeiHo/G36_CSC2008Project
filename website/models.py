89423211fe373b4b5c6feaadf90cc89c0c34265e
DarrenQSW <2717955Q@student.gla.ac.uk>
2023-03-17 13:19:43 +0800

    Models and donation history

--- website/models.py
+++ website/models.py
@@ -12,7 +12,8 @@ class Users(db.Model):
     Password = db.Column(db.String(75), nullable=False)
     Role = db.Column(db.String(10), nullable=False)
 
-    
+    role_relation = db.relationship('Roles', backref='users')
+    donor = db.relationship('Donors', backref='user', uselist=False)
 
     # def create(self):
     #     db.session.add(self)
@@ -24,5 +25,69 @@ class Users(db.Model):
 
     def check_password(self, password):
         return bcrypt.checkpw(password, self.Password)
+    
+class Roles(db.Model):
+    RoleID = db.Column(db.Integer, primary_key=True)
+    RoleName = db.Column(db.String(10), unique=True)
 
+    users = db.relationship('Users', backref='role')
+    staff = db.relationship('Staff', backref='role')
 
+class MedicalConditions(db.Model):
+    MedicalConditionID = db.Column(db.Integer, primary_key=True)
+    Name = db.Column(db.String(75), unique=True)
+
+    donors = db.relationship('Donors', backref='medical_condition')
+
+class Donors(db.Model):
+    DonorID = db.Column(db.Integer, primary_key=True)
+    UserID = db.Column(db.Integer, db.ForeignKey('users.UserID'))
+    DonorName = db.Column(db.String(50))
+    DonorAge = db.Column(db.Integer)
+    DonorGender = db.Column(db.String(1))
+    DonorWeight = db.Column(db.Float)
+    BloodType = db.Column(db.String(3))
+    DonorAddress = db.Column(db.String(250))
+    DonorMedicalHistory = db.Column(db.String(50), db.ForeignKey('medical_conditions.Name'))
+
+    appointments = db.relationship('Appointment', backref='donor')
+    donations = db.relationship('Donations', backref='donor')
+
+class Donations(db.Model):
+    DonationID = db.Column(db.Integer, primary_key=True)
+    DonorID = db.Column(db.Integer, db.ForeignKey('donors.DonorID'))
+    DonationDate = db.Column(db.Date, nullable=False)
+    Quantity = db.Column(db.Integer, nullable=False)
+    Location = db.Column(db.String(50), nullable=False)
+
+class Appointment(db.Model):
+    AppointmentID = db.Column(db.Integer, primary_key=True)
+    Date = db.Column(db.DateTime, nullable=False)
+    DonorID = db.Column(db.Integer, db.ForeignKey('donors.DonorID'))
+    DonationCenterID = db.Column(db.Integer, db.ForeignKey('donation_center.DonationCenterID'))
+    SlotID = db.Column(db.Integer, db.ForeignKey('slots.SlotID'))
+
+class DonationCenter(db.Model):
+    DonationCenterID = db.Column(db.Integer, primary_key=True)
+    Name = db.Column(db.String(50), nullable=False)
+    Address = db.Column(db.String(100), nullable=False)
+
+    appointments = db.relationship('Appointment', backref='donation_center')
+    slots = db.relationship('Slots', backref='donation_center')
+
+class Slots(db.Model):
+    SlotID = db.Column(db.Integer, primary_key=True)
+    StartTime = db.Column(db.Time, nullable=False)
+    EndTime = db.Column(db.Time, nullable=False)
+    Max_Bookings = db.Column(db.Integer, nullable=False)
+    Booked_Count = db.Column(db.Integer, nullable=False)
+    DonationCenterID = db.Column(db.Integer, db.ForeignKey('donation_center.DonationCenterID'))
+
+    appointments = db.relationship('Appointment', backref='slot')
+
+class Staff(db.Model):
+    StaffID = db.Column(db.Integer, primary_key=True)
+    Name = db.Column(db.String(50), nullable=False)
+    Email = db.Column(db.String(50), nullable=False, unique=True)
+    Password = db.Column(db.String(75), nullable=False)
+    Role = db.Column(db.String(10), db.ForeignKey('roles.RoleName'))
--- website/views.py
+++ website/views.py
@@ -1,69 +1,15 @@
 #Not Completed, got Errors
 
-import uuid
 from . import db
-from .models import Users
+from .models import *
 from flask import Blueprint, Flask, render_template, request, redirect, session, flash
-import bcrypt
+from sqlalchemy import select
 
 views = Blueprint('views', __name__)
-
-# app = Flask(__name__)
-# app.secret_key = "secret_key"
-
-# # Database configuration
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blood_donation.db'
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-# db.init_app(app)
 
 @views.route('/')
 def home():
     return render_template('index.html')
-
-# @views.route('/register', methods=['GET', 'POST'])
-# def register():
-#     if request.method == 'POST':
-#         name = request.form['name']
-#         email = request.form['email']
-#         phone = request.form['phone']
-#         password = bcrypt.hashpw(request.form['password'].encode('utf-8'), bcrypt.gensalt())
-#         role = 'user'
-#         userid = str(uuid.uuid4())
-
-#         user = Users(UserID=userid,Name=name, Email=email, Phone=phone, Password=password, Role=role)
-#         db.session.add(user)
-#         db.session.commit()
-
-#         session['email'] = email
-#         return redirect('/')
-#     else:
-#         return render_template('register.html')
-
-# @views.route('/login', methods=['GET', 'POST'])
-# def login():
-#     if request.method == 'POST':
-#         email = request.form['email']
-#         password = request.form['password'].encode('utf-8')
-
-#         user = Users.get_by_email(email)
-        
-
-#         if user and user.check_password(password):
-#             session['email'] = email
-
-#             # Show a notification of successful login
-#             flash('Login successful', 'success')
-#             return render_template('main.html')
-#         else:
-#             return render_template('login.html', error='Invalid email or password')
-#     else:
-#         return render_template('login.html')
-
-# @views.route('/logout')
-# def logout():
-#     session.pop('email', None)
-#     return redirect('/')
 
 @views.route('/main')
 def mainpage():
@@ -79,4 +25,14 @@ def startcheck():
 
 @views.route('/donationhistory')
 def donationhistory():
-    return render_template('donationhistory.html')
+     # Define a query to fetch only the required columns
+    stmt = select(Donations.DonationDate, Donations.Quantity, Donations.Location)
+    
+    # Execute the query
+    result = db.session.execute(stmt)
+    
+    # Fetch all records as a list of namedtuples
+    donations = result.fetchall()
+
+    # Pass the donations data to the donationhistory.html template
+    return render_template('donationhistory.html', donations=donations)
