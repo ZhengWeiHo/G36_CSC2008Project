44c28575e4b2e6eca264b4ee1d21c028203c96f6
DarrenQSW <2717955Q@student.gla.ac.uk>
2023-03-23 17:31:41 +0800

    In prog

--- website/auth.py
+++ website/auth.py
@@ -44,43 +44,57 @@ def login():
 
 @auth.route('/register', methods=['GET', 'POST'])
 def register():
+    medical_conditions = MedicalConditions.query.all()
+    print("Medical conditions:", medical_conditions)
     if request.method == 'POST':
         name = request.form['name']
         email = request.form['email']
         dob = request.form['dob']
+        gender = request.form['gender']
         phone = request.form['phone']
         weight = request.form['weight']
         password = bcrypt.hashpw(request.form['password'].encode('utf-8'), bcrypt.gensalt())
         medical_history_id = request.form['medical_history']
 
+        blood_type = request.form['blood_type']
+        address = request.form['address']
+
         # Calculate age from date of birth
-        birthdate = datetime.datetime.strptime(dob, '%Y-%m-%d')
-        today = datetime.date.today()
-        age = today.year - birthdate.year - ((today.month, today.day) < (birthdate.month, birthdate.day))
+        age = calculate_age(dob)
 
         # Check whether email already exists
         user = Users.get_by_email(email)
         if user:
             flash('Email already exists', category='error')
-            return render_template('register.html')
+            return render_template('register.html', medical_conditions=medical_conditions)
 
         # Create user and donor objects
         user = Users(Name=name, Email=email, Phone=phone, Password=password, Role=1)
-        donor = Donors(DonorName=name, DonorAge=age, DonorWeight=weight, UserID=user.UserID, BloodType=None)
+        db.session.add(user)
+        db.session.commit()
+
+        donor = Donors(DonorName=name, DonorAge=age, DonorGender=gender, DonorWeight=weight, UserID=user.UserID, BloodType=blood_type, DonorAddress=address)
 
         # Add medical history to donor if selected
         if medical_history_id != 'None':
-            medical_condition = MedicalConditions.query.filter_by(MedicalConditionID=medical_history_id).first()
-            donor.DonorMedicalHistory = medical_condition.MedicalConditionID
+            medical_condition = MedicalConditions.query.filter_by(Name=medical_history_id).first()
+            print("Medical condition:", medical_condition)
+            if medical_condition:
+                print("Medical condition ID:", medical_condition.MedicalConditionID)
+                donor.DonorMedicalHistory = medical_condition.MedicalConditionID
+            else:
+                flash('Invalid medical condition selected', category='error')
+                return render_template('register.html', medical_conditions=medical_conditions)
 
-        db.session.add(user)
+        # Update donor with user ID
+        donor.UserID = user.UserID
         db.session.add(donor)
         db.session.commit()
 
         session['email'] = email
         return redirect('/login')
     else:
-        return render_template('register.html')
+        return render_template('register.html', medical_conditions=medical_conditions)
 
 @auth.route('/logout')
 def logout():
